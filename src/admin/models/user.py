d18c89761c168a6293b4bd6b7f812ecc1a22c881
Matvii Strechen <mvstrechen@gmail.com>
2020-08-10 02:14:49 +0300

    Add "role" column for user mode

--- src/admin/models/user.py
+++ src/admin/models/user.py
@@ -1,8 +1,16 @@
 from admin.app import db
 
 
+class UserRole:
+    ADMIN = 'admin'
+    USER = 'user'
+
+    ALL_ROLES = [ADMIN, USER]
+
+
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.INT, primary_key=True, autoincrement=True)
     full_name = db.Column(db.VARCHAR(255))
     email = db.Column(db.VARCHAR(255))
+    role = db.Column(db.Enum(*UserRole.ALL_ROLES))
