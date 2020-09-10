c9d3649023b7a4abc27be2de34b1c1bece5a25db
m.shcherbina <mshcherbina99@gmail.com>
2020-09-10 23:53:48 +0300

    provided exception handling for parsing news lists

--- src/scraping/tasks/parse_news_list.py
+++ src/scraping/tasks/parse_news_list.py
@@ -1,3 +1,5 @@
+from selenium.common.exceptions import WebDriverException
+
 from admin import celery
 from admin.models.site_parsers import SiteParser
 from admin.models.articles import Article
@@ -22,11 +24,10 @@ def fetch_site_parser_by_id(site_parser_id):
 
 
 def parse_config(site_parser):
-    logger.info(site_parser.rules)
     if site_parser.syntax == "json":
         rules = json.loads(site_parser.rules)
     else:
-        raise Exception("Yaml parser configuration isn't supported yet")
+        raise ValueError("Yaml parser configuration isn't supported yet")
 
     list_rules = rules['list']
     list_url = list_rules['url']
@@ -42,7 +43,7 @@ def parse_config(site_parser):
     return list_url, item_xpath, item_link_subpath, next_xpath, article_rules
 
 
-def is_article_exists(link):
+def does_article_exist(link):
     session = Session()
     article_exists = session.query(Article).filter_by(url=link).count() > 0
     session.close()
@@ -56,25 +57,46 @@ def parse_news_list_task(site_parser_id):
     logger.info("Parsing news list for site parser {}".format(site_parser_id))
     site_parser = fetch_site_parser_by_id(site_parser_id)
     if not site_parser.has_newslist_parser:
-        return dict(result='SUCCESS', comment="Nothing to parse with site parser {}".format(site_parser.parser_id))
+        return dict(result='SUCCESS', comment="Nothing to parse with site parser {}".format(site_parser_id))
 
-    list_url, item_xpath, item_link_subpath, next_xpath, article_rules = parse_config(site_parser)
+    try:
+        list_url, item_xpath, item_link_subpath, next_xpath, article_rules = parse_config(site_parser)
+    except ValueError:
+        return dict(result='FAILURE', comment="Error have occurred during parsing rules for site parser {}"
+                    .format(site_parser_id))
 
     driver = get_driver()
+
+    limit = 10
 
     next_url = list_url
     fetched_articles = 0
     reached_parsed_article = False
-    limit = 10
-    while fetched_articles < limit:
-        page = driver.get(next_url)
-        next_url = driver.find_element_by_xpath(next_xpath).get_attribute("href")
+    has_next_page = True
+    while fetched_articles < limit and has_next_page:
+        try:
+            page = driver.get(next_url)
+        except WebDriverException:
+            return dict(result='FAILURE', comment="Failed to get ".format(next_url))
 
-        news_items = driver.find_elements_by_xpath(item_xpath)
+        try:
+            next_url = driver.find_element_by_xpath(next_xpath).get_attribute("href")
+        except WebDriverException:
+            has_next_page = False
+
+        try:
+            news_items = driver.find_elements_by_xpath(item_xpath)
+        except WebDriverException:
+            return dict(result='FAILURE', comment="Failed to get news items")
+
         for item in news_items:
-            link = item.find_element(By.XPATH, ".//" + item_link_subpath).get_attribute("href")
+            try:
+                link = item.find_element(By.XPATH, ".//" + item_link_subpath).get_attribute("href")
+            except WebDriverException:
+                logger.info("Failed to extract article link from news list item")
+                continue
 
-            reached_parsed_article |= is_article_exists(link)
+            reached_parsed_article |= does_article_exist(link)
             if reached_parsed_article:
                 break
 
