5fc2320ee5442e55986ac57fef7ca8b2db2780a3
Matvii Strechen <mvstrechen@gmail.com>
2020-09-12 03:45:04 +0300

    Articles views: show title & content

--- src/admin/views/articles.py
+++ src/admin/views/articles.py
@@ -1,3 +1,5 @@
+from flask_admin.tools import rec_getattr
+
 from admin.models.articles import Article
 from admin.utils.views import PatchedModelView
 
@@ -10,3 +12,10 @@ class ArticlesView(PatchedModelView):
     can_delete = False
     can_create = False
     can_edit = False
+
+    column_list = ['site_id', 'language', 'url', 'text.title', 'text.content']
+    column_details_list = column_list
+    column_formatters = {
+        'text.content': lambda c, v, m, n: rec_getattr(m, n, '')[:300]
+    }
+    column_formatters_detail = {}
