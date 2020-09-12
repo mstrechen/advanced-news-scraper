8fef78df28f5334203417c49cdc8fdfcbd081308
= <=>
2020-09-12 10:34:10 +0300

    Basic views for sites and site parsers

--- src/admin/views/sites/site_parsers.py
+++ src/admin/views/sites/site_parsers.py
@@ -1,3 +1,6 @@
+from flask_wtf import Form
+from wtforms import HiddenField
+
 from admin.models.site_parsers import SiteParser
 from admin.utils.views import PatchedModelView
 
@@ -8,5 +11,35 @@ class SiteParsersView(PatchedModelView):
     can_view_details = True
 
     column_editable_list = []
-    can_create = False
+    can_create = True
+    can_edit = True
     can_delete = False
+
+    form_excluded_columns = ['has_newslist_parser', 'has_article_parser', 'creator_id', 'created']
+    form_choices = {
+        'syntax': [
+            ('json', 'json'),
+            # ('yaml', 'yaml'),
+        ],
+        'type': [
+            # ('dynamic', 'dynamic'),
+            ('static', 'static'),
+            # ('rest_api', 'rest_api'),
+        ],
+    }
+
+    def get_create_form(self):
+        form = super(SiteParsersView, self).get_create_form()
+
+        class Form(form):
+            has_newslist_parser = HiddenField()
+            has_article_parser = HiddenField()
+            creator_id = HiddenField()
+
+        return Form
+
+    def create_model(self, form: Form):
+        form.has_newslist_parser.data = True
+        form.has_article_parser = True
+        form.creator_id.data = current_user.id
+        return super(SiteParsersView, self).create_model(form)
--- src/admin/views/sites/sites.py
+++ src/admin/views/sites/sites.py
@@ -1,6 +1,20 @@
+import requests
+
+from wtforms import ValidationError
+import wtforms.validators as validators
+
 from admin.models.sites import Site
 from admin.utils.views import PatchedModelView
 
+class UrlAvailabilityValidator:
+    @classmethod
+    def validator(cls, form, field):
+        try:
+            url = field.data
+            resp = requests.head(url)
+            resp.raise_for_status()
+        except Exception as e:
+            raise ValidationError("Provided invalid or inaccessible URL")
 
 class SitesView(PatchedModelView):
     CONFIG_MODEL = Site
@@ -8,5 +22,10 @@ class SitesView(PatchedModelView):
     can_view_details = True
 
     column_editable_list = []
-    can_create = False
-    can_delete = False
+    can_create = True
+    can_delete = True
+
+    form_args = {
+        'base_url': dict(validators=[UrlAvailabilityValidator.validator]),
+    }
+    form_excluded_columns = ['created']
